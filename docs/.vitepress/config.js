74ef53b20e8b4c265147b74d85ca69de72080018
Abudu <abudulin@foxmail.com>
2023-01-07 23:07:59 +0800

    change statistic

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,13 +4,7 @@ module.exports = {
     description: '一个快速、强大、漂亮的在线 Hexo 管理器',
     head: [
         ['link', { rel: 'icon', href: '//unpkg.com/qexo-static@1.4.0/assets/img/brand/favicon.ico' }],
-        ['script', {},`var _hmt = _hmt || [];
-        (function() {
-          var hm = document.createElement("script");
-          hm.src = "https://hm.baidu.com/hm.js?d6e67b4d53522eb65be1d757678be9d5";
-          var s = document.getElementsByTagName("script")[0]; 
-          s.parentNode.insertBefore(hm, s);
-        })();`]
+        ['script', {},`!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JvwJATuEYs3V2Qun",ck:"JvwJATuEYs3V2Qun",autoTrack:true,hashMode:true});`]
     ],
     lastUpdated: true,
     markdown: {
